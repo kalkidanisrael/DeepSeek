5fe27940764f732d0444ae36d71a6a19935207a3
Kalizra <kalkidanisrael6@gmail.com>
2026-05-03 11:04:02 +0300

    first commit

---
+++ app/api/clerk/route.js
@@ -0,0 +1,39 @@
+import {webhook} from "svix";
+import connectDB from "@/config/db";
+import User from "@/models/User";
+export async function POST(req){
+    const wh=new webhook(process.env.SIGNING_SECRET)
+    const headerPayload=await headers()
+    const svixHeaders={
+        "svix":headerPayload.get("svix-id"),
+        "svix-signature":headerPayload.get("svix-signature"),
+    };
+    // Get the payload and verify it 
+    const payload=await req.json();
+    const body=JSON.stringify(payload)
+    const {data,type} =wh.verify(body,svixHeaders)
+//Prepare the user data to be saved in the database 
+const  useData={
+    _id:data.id,
+    email:data.email_addresses[0].email_address,
+    name:`${data.first_name} ${data.last_name}`,
+    image:data.image_ulr,
+
+};
+await connectDB();
+switch(type){
+    case 'user.created':
+        await User.created(uerData)
+        break;
+        case 'user.updated':
+           await User.findByIdAndUpdate(data.id,userData)
+            break;
+        case 'user.deleted':
+           await User.findByIdAndDelete(data.id)
+            break;    
+        default :
+        break
+}
+return NextRequest.json({message:"Event received"});
+
+}
---
+++ components/PromptBox.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import Image from 'next/image'
+import {assets} from '../assets/assets'
+import { useState } from 'react'
+const PromptBox = ({isLoading,setIsLoading}) => {
+    const [prompt,setPrompt]=useState(' ')
+  return (
+   <form className={`w-full ${false?"max-w-3x1":"max-w-2x1"} bg-[#404045] p-4 rounded-3x1 mt-4 transition-all`}> 
+   <textarea
+   className='outline-none w-full resize-none overflow-hidden break-words bg-transparent'
+   rows={2}
+   onChange={(e)=>setPrompt(e.target.value ) } value={prompt}
+   placeholder='Message DeepSeek' required/>
+   <div className='flex items-center justify-between text-sm'>
+    <div className='flex items-center gap-2'>
+        <p className='flex items-center gap-2 text-xs border border-gray-300/40 px-2 py-1 rounded-full cursor-pointer hover:bg-gray-500/20 transition'>
+            <Image src={assets.deepthink_icon} alt=''/>
+            DeepThink (R1)
+        </p>
+        <p className='flex items-center gap-2 text-xs border border-gray-300/40 px-2 py-1 rounded-full cursor-pointer hover:bg-gray-500/20 transition'>
+            <Image className='h-5' src={assets.deepthink_icon} alt=''/>
+            Search
+        </p>
+    </div>
+    <div className='flex items-center gap-2 '>
+        <Image src={assets.pin_icon} alt='' className='w-4 cursor-pointer'/>
+            <button className={`${prompt? "bg-primary":"bg-[#71717a]"} rounded-full p-2 cursor-pointer`}> 
+                <Image src={prompt? assets.arrow_icon:assets.arrow_icon_dull} alt='' className='w-4 cursor-pointer'/>
+            </button>
+    </div>
+   </div>
+
+   </form>
+  )
+}
+
+export default PromptBox
