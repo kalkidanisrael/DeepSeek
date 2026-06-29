fb821582f85e0658b1da299f557db3bee461d4cf
Kalizra <kalkidanisrael6@gmail.com>
2026-06-29 17:16:15 +0300

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import { AppContextProvider } from "@/context/AppContext";
+import { AppContextProvider } from "@/context/Appcontext";
 
 const inter = Inter({
   variable: "--font-inter",
