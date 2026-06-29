3b98af2515132c6a609afde49e25bd42827d3859
Kalizra <kalkidanisrael6@gmail.com>
2026-06-29 17:06:40 +0300

    fix layout and update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,33 @@
 import type { Metadata } from "next";
-import { Inter} from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
-import {ClerkProvider} from "@clerk/nextjs"
-import {AppContextProvider} from "@/context/Appcontext"
+import { ClerkProvider } from "@clerk/nextjs";
+import { AppContextProvider } from "@/context/AppContext";
+
 const inter = Inter({
-  variable: "--font-inder",
+  variable: "--font-inter",
   subsets: ["latin"],
 });
-
 
 export const metadata: Metadata = {
   title: "DeepSeek",
   description: "FullStack Project",
 };
 
-export default function RootLayout({children}){
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <ClerkProvider>
-     <AppContextProvider>
       <html lang="en">
         <body className={`${inter.className} antialiased`}>
-              {children}
+          <AppContextProvider>
+            {children}
+          </AppContextProvider>
         </body>
       </html>
-       </AppContextProvider>
     </ClerkProvider>
-  )
+  );
 }
